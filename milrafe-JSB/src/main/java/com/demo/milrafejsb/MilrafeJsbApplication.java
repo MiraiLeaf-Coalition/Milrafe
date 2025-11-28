fb72c01610176c5038e4b7c839cf445d1367277c
Lelead <13212038779@163.com>
2025-11-28 12:40:22 +0800

    添加后端

---
+++ milrafe-JSB/src/main/java/com/demo/milrafejsb/MilrafeJsbApplication.java
@@ -0,0 +1,13 @@
+package com.demo.milrafejsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilrafeJsbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilrafeJsbApplication.class, args);
+    }
+
+}
---
+++ milrafe-JSB/src/test/java/com/demo/milrafejsb/MilrafeJsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.milrafejsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilrafeJsbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
